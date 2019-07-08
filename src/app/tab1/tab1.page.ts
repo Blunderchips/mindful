7395caba553b870691975c8752c31a41b4232a65
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-07-08 13:13:07 +0200

    Inserting moods to firebase

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  userUid = '1234';
+  moods = [
+    {
+      mood: 'happy',
+      icon: 'happy'
+    },
+    {
+      mood: 'sad',
+      icon: 'sad'
+    },
+    {
+      mood: 'depressed',
+      icon: 'sad'
+    },
+    {
+      mood: 'tierd',
+      icon: 'sad'
+    }
+  ];
 
+  constructor(private afs: AngularFirestore) { }
+
+  ngOnInit(): void {
+
+  }
+
+  postMood(mood: string) {
+    let uid = this.afs.createId();
+    this.afs.collection(this.userUid + '_data').add(
+      {
+        user: this.userUid,
+        uid: uid,
+        mood: mood,
+        date: new Date()
+      }
+    );
+  }
 }
