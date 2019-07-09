3bc48e634ca8f84250ad08799fd9e7c9b5d24f1b
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-07-09 11:58:05 +0200

    Generate about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'splash', loadChildren: './splash/splash.module#SplashPageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'about', loadChildren: './about/about.module#AboutPageModule' }
 ];
 @NgModule({
   imports: [
