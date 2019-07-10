59dd532722eb9aa5ea2e75d699e03260a761f1f6
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-07-10 19:30:42 +0200

    Product icons

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -22,7 +22,7 @@ export class Tab4Page implements OnInit {
       title: 'Bank',
       // tslint:disable-next-line: max-line-length
       descriptoin: 'It’s time to bank healthier with Discovery. We\'ll work with you to help you improve your Vitality Money points and status. You’ll get personal money goals that are based on your unique financial profile. Through your personal money goals, you can improve your financial knowledge, work towards increasing your savings, and track your progress easily. Join now and ensure that you reach your Vitality Retirement Age.',
-      img: '',
+      img: 'https://i.imgur.com/5pHEOwd.png',
       url: 'https://www.discovery.co.za/bank/faq'
     },
     medical: {
@@ -36,35 +36,35 @@ export class Tab4Page implements OnInit {
       title: 'Life Insurance',
       // tslint:disable-next-line: max-line-length
       descriptoin: 'The Smart PayBack Fund on Comprehensively Integrated Smart Life Plans, allows you to receive up to 100% of your qualifying Smart Life Plan premiums back for managing your health and wellness and driving well. With the information we receive from Vitality and Vitality Drive, we can measure your level of health and wellness and your driving behaviour. Utilising this information, we will add a percentage of your premiums to your Smart PayBack Fund each year.',
-      img: '',
+      img: 'https://imgur.com/0saSvK0.png',
       url: 'https://www.discovery.co.za/life-insurance/'
     },
     gap: {
       title: 'Gap Cover',
       // tslint:disable-next-line: max-line-length
       descriptoin: 'Covers tariff shortfalls on approved in-hospital specialist claims, specific out-of-hospital costs related to approved hospital admission and shortfalls on approved emergency international claims while travelling outside of South Africa. You also get cover for co-payments on approved endoscopies, MRI and CT scans, and cancer-related claims. Your cover depends on the Discovery Gap Cover option you choose. An overall annual limit of R157 500 applies to each person.',
-      img: '',
+      img: 'https://imgur.com/uGfsP6u.png',
       url: 'https://www.discovery.co.za/gap/gap-cover-products'
     },
     investment: {
       title: 'Investment',
       // tslint:disable-next-line: max-line-length
       descriptoin: 'Our investment solutions make investing more rewarding for you. Whether you’re putting money away for a trip to France, making sure you’ll have enough to pay for your children’s university fees, or investing for retirement, we make your investment journey easier.  Some of the reasons people don’t reach their investment goals include not saving enough, not starting early enough, not preserving their savings and withdrawing too much in retirement. We understand these challenges, and we make investing more rewarding when you take steps to improve your financial behaviour.',
-      img: '',
+      img: 'https://imgur.com/616XFhN.png',
       url: 'https://www.discovery.co.za/investments/invest-with-discovery'
     },
     vitality1: {
       title: 'Vitality',
       // tslint:disable-next-line: max-line-length
       descriptoin: 'Half-price movies at Ster-Kinekor, savings on your monthly gym fees at Virgin Active or Planet Fitness, local and international travel rewards with British Airways, kulula.com, Emirates and Qantas, cash back on your groceries at Pick n Pay or Woolworths and more. Being healthier, driving well and banking well has never been more rewarding.',
-      img: '',
+      img: 'https://imgur.com/bia53Hg.png',
       url: 'https://www.discovery.co.za/vitality/join-today'
     },
     vitality2: {
       title: 'Vitality',
       // tslint:disable-next-line: max-line-length
       descriptoin: 'Save up to 75% on flights in terms of local, regional and international destinations with British Airways, kulula.com, Emirates and Qantas. Vitality Health members save up to 35% on local, regional and international flights. Boost your Vitality travel saving by up to an additional 40% as a Vitality Health member and a Discovery Bank client with the Vitality Money programme.',
-      img: '',
+      img: 'https://imgur.com/bia53Hg.png',
       url: 'https://www.discovery.co.za/vitality/join-today'
     }
   };
